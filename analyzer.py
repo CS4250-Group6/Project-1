36fad3283452ce4a3e2884bc731e036db74dc17d
Andr3wD <37052064+Andr3wD@users.noreply.github.com>
2021-09-23 20:19:31 -0700

    Changed probability to frequency to match project requirements

--- analyzer.py
+++ analyzer.py
@@ -23,28 +23,26 @@ def zipfsLaw():
     )  # Make an array, sorted by word rank. So, index is rank.
 
     # Calculate the probability of each word
-    wordProbabilities = []
+    wordFrequencies = []
     for w in wordRanks:
-        probability = wordCounter[w] / totalWordCount
-        wordProbabilities.append(probability)
+        wordFrequencies.append(wordCounter[w])
 
     # for i in range(len(wordRanks)):
     #     print(i*wordProbabilities[i])
 
     # Plot the Ziph's Law line.
     ziphLineY = []
-    ziphLineY.extend([0.1 / (x + 1) for x in range(len(wordRanks))])
+    K = 0.1 * totalWordCount
+    ziphLineY.extend([K / (x + 1) for x in range(len(wordRanks))])
     plt.plot(range(1, len(wordRanks) + 1), ziphLineY, label="Zipf", c="g")
 
     # Graph the probabilities vs rank:
-    plt.scatter(
-        range(1, len(wordRanks) + 1), wordProbabilities, s=5, label="Data", c="r"
-    )
+    plt.scatter(range(1, len(wordRanks) + 1), wordFrequencies, s=5, label="Data", c="r")
     # plt.locator_params(nbins=10)
 
     # Naming stuff
     plt.xlabel("Rank\n(by decreasing frequency)")
-    plt.ylabel("Probability\n(of occurrence)")
+    plt.ylabel("Frequency")
     plt.title("Zipf's Law")
 
     # Make x and y scale log base 10
