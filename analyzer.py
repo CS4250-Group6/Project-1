3bcdf1d853229dc615b09ff1646ed83ab91aba8b
rosalindach28 <rosalindaa.chamale@gmail.com>
2021-10-05 21:55:32 -0700

    Cleaned up Heap's Law code, removed unnecessary functions

--- analyzer.py
+++ analyzer.py
@@ -38,33 +38,17 @@ def zipfsLaw():
 
 
 def heapsLaw():
-    # num_of_words = getWordCounter()
-    # totalWordCount = sum(num_of_words.values()) # total number of words
-
-    # Make arrays to plot data
-    unique_so_far = []
-    for word in find_unique_words():
-        unique_so_far.append(word)
-
-    totalWords_so_far = []
-    # for w in allWords(): ?
-    for w in range(0, len(unique_so_far)):
-        totalWords_so_far.append(w)
-
     vocabList = getVocabList()
 
     # Graph Labels
-    plt.xlabel("Words in Collection")  # this is N; total number of words
-    plt.ylabel("Words in Vocabulary")  # this is Vocab size (number of unique words)
+    plt.xlabel("Words in Collection")  # this is the total number of words
+    plt.ylabel("Words in Vocabulary")  # this is the vocabulary size (number of unique words)
     plt.title("Heap's Law")
 
-    # define scale of x and y axis on graph
-    # plt.yscale("log", base=10)
-    # plt.xscale("log", base=10)
+    # scale of x and y axis on graph is normal
 
-    # plot corresponding data
-    # Plot the Heap's Law line
-    plt.plot(range(len(vocabList)), vocabList, label="Data", c="g")
+    # plot corresponding data using a line to represent Heap's Law implementation
+    plt.plot(range(len(vocabList)), vocabList, label="Data", c="b")
     plt.legend()  # Display the legend
     plt.show()  # show graph
 
@@ -78,7 +62,7 @@ def getVocabList() -> list[int]:
     for name in fileNames:
         if name[-5:] == ".html":  # Only open files labeled .html
             filePath = os.path.join(path, name)
-            with open(filePath) as f:
+            with open(filePath, encoding="utf8") as f:
                 rawHTML = f.read()  # Get the raw html
 
                 # Get only the text in the raw html, then make it all lowercase. Then merge all of the texts into a single string.
@@ -96,65 +80,6 @@ def getVocabList() -> list[int]:
                     vocabList.append(len(uniqueWords))
 
     return vocabList
-
-
-def find_unique_words():
-    fileNames = os.listdir("repository")
-    # Parse through all file names in ./repository/
-    for name in fileNames:
-        if name[-5:] == ".html":  # Only open files labeled .html
-            filePath = os.path.join("repository", name)
-            with open(filePath, encoding="utf8") as f:
-                rawHTML = f.read()  # Get the raw html
-
-                # Get only the text in the raw html, then make it all lowercase. Then merge all of the texts into a single string.
-                bs = BeautifulSoup(rawHTML, "lxml")
-                words = bs.getText(" ").split()
-                words = [
-                    x.strip("0123456789~`^%$#@&*?!.;:'()[]{},/|\\><+=_- \"").lower()
-                    for x in words
-                ]
-                unique_words = []
-                for x in words:
-                    if x not in unique_words:
-                        unique_words.append(x)
-                return unique_words
-
-
-def unique_word_count():
-    unique_count = len(find_unique_words())
-    return unique_count
-
-
-# used to fill the totalWords_sofar array, not sure if useful
-# returns bigger number than totalWordCount
-def allWords():
-    """
-    List of words from all the .html files in the ./repository/ folder.
-
-    Parameters:
-        TODO language (str): a specific language to get the html files for.
-    """
-    fileNames = os.listdir("repository")
-    wordCounter = Counter()
-    # Parse through all file names in ./repository/
-    for name in fileNames:
-        if name[-5:] == ".html":  # Only open files labeled .html
-            filePath = os.path.join("repository", name)
-            with open(filePath) as f:
-                rawHTML = f.read()  # Get the raw html
-
-                # Get only the text in the raw html, then make it all lowercase. Then merge all of the texts into a single string.
-                bs = BeautifulSoup(rawHTML, "lxml")
-                words = bs.getText(" ").split()
-                words = [
-                    x.strip("0123456789~`^%$#@&*?!.;:'()[]{},/|\\><+=_- \"").lower()
-                    for x in words
-                ]
-                all_words = []
-                for a in words:
-                    all_words.append(a)
-                return all_words
 
 
 def getWordCounter() -> Counter:
@@ -191,6 +116,6 @@ def getWordCounter() -> Counter:
 
 if __name__ == "__main__":
     global selectedLanguage
-    selectedLanguage = "fr"
+    selectedLanguage = "es"
     # zipfsLaw()
     heapsLaw()
